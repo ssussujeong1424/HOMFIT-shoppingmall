58a6741c4f2bcde40ab9b4e143cc827321ee2e7e
수정 <ssussujeong1424@gmail.com>
2026-09-02 16:11:05 +0900

    9/2 푸터까지

---
+++ js/product-control.js
@@ -0,0 +1,30 @@
+const saleUlTag = document.querySelector('.sale');
+let result = productArray.map(product => {
+    return `<li>
+                        <a href="#">
+                            <figure>
+                                <img src="./img/${product.pthumbFileName}" alt="${product.pname}">
+                            </figure>
+                            <div class="sale-txt">
+                                <h4 class="title-1">${product.pname}</h4>
+                                <p class="desc-1">${product.pdesc}</p>
+                                <div class="pay-frame">
+                                    <div class="pay-original">
+                                        <span>${formatMoney(product.price)}</span>원
+                                    </div>
+                                    <div class="pay-discount">
+                                        <div class="discount">${Math.round(product.pdiscount*100)}%</div>
+                                        <div class="pay"><b>${formatMoney(product.price*product.pdiscount)}</b>원</div>
+                                    </div>
+                                </div>
+                                <span class="like-badge noab"><img src="./img/heart-1.svg" alt="좋아요하트">${product.plikes}</span>
+                            </div>
+                        </a>
+                    </li>`
+
+}).join('')
+
+saleUlTag.innerHTML = result
+
+function formatMoney(value){
+    return Number(value).toLocaleString('ko-KR');}
---
+++ js/product.js
@@ -0,0 +1,45 @@
+let productArray = [
+    {
+        pid: 0,
+        pname: '오브 패브릭 암체어',
+        pdesc: `부드러운 패브릭과 안정감 있는 곡선 디자인으로 어떤 공간에서도 편안한 휴식을         선사합니다.`,
+        price: 459000,
+        pdiscount: 0.15,
+        priceDiscount: 390150,
+        plikes: 238,
+        pthumbFileName: 'month1.jpg'
+    },
+    {
+        pid: 1,
+        pname: '라운드 사이드 테이블 500',
+        pdesc: `컴팩트한 사이즈로 소파 옆이나 침대 옆에 두기 좋아요. 
+                수납 공간이 있어 실용성까지 챙겼습니다.`,
+        price: 239000,
+        pdiscount: 0.1,
+        priceDiscount: 215100,
+        plikes: 164,
+        pthumbFileName: 'month2.jpg'
+    },
+    {
+        pid: 2,
+        pname: '슬림 키친 수납장 800',
+        pdesc: `좁은 주방 공간도 깔끔하게 정리할 수 있는 슬림디자인.`,
+        price: 649000,
+        pdiscount: 0.2,
+        priceDiscount: 519200,
+        plikes: 341,
+        pthumbFileName: 'month3.jpg'
+    },
+    {
+        pid: 3,
+        pname: '코지 5단 서랍장',
+        pdesc: `넉넉한 5단 수납으로 옷과 소품을 깔끔하게 정리할 수 있어요. 
+                매트한 컬러감과 미니멀한 디자인이 특징입니다.`,
+        price: 529000,
+        pdiscount: 0.18,
+        priceDiscount: 434780,
+        plikes: 287,
+        pthumbFileName: 'month4.jpg'
+    },
+
+]
